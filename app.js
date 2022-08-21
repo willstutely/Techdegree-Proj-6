5cb21ee442814e9a32ae18f265d61b821f03ce02
willstutely <jonathanandrewdrake@gmail.com>
2022-08-21 01:19:57 -0500

    updating error handling

--- app.js
+++ app.js
@@ -16,18 +16,32 @@ app.set('view engine', 'pug');
 const routeIndex = require('./routes/index')
 const routeProject = require('./routes/projects');
 const routeAbout = require('./routes/about')
-app.use(routeIndex)
-app.use(routeProject)
-
-
+app.use(routeIndex);
+app.use(routeProject);
+app.use(routeAbout);
 
 // Error handling
-// app.use((req, res, next) => {
-//     const err = new Error('Not Found');
-//     err.status = 404;
-//     next(err);
-//   });
+app.use((req, res, next) => {
+    const err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+  });
 
+  app.use((err, req, res, next) => {
+    res.locals.error = err;
+       
+    if (err.status === undefined) {
+        err.status = 500
+        err.message = "Whoops... the page you're looking for can't be found (Error 500)"
+        res.status(err.status)
+        res.render('error');
+    } else {
+        err.message = "Whoops... the page you're looking for can't be found (Error 404)"
+        res.status(err.status)
+        res.render('page-not-found');
+    }
+    
+  })
 
 app.listen(3000, () => {
     console.log('The app is running on localhost:3000')
--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,6 @@ const { projects } = require('../data/data.json');
 
 
 router.get('/', (req, res) => {
-    // const projectId = req.params.id;
-    // const project = projects.find( ({ id }) => id === +projectId );
     res.render('index', {projects})
 });
 
